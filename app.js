5b028db59b9562d6b84927523cdd87730b404a5a
diciommoriccardo <riccardo.diciommo1@gmail.com>
2022-06-21 17:00:51 +0200

    fixed proxy

--- app.js
+++ app.js
@@ -37,11 +37,19 @@ app.use(passport.session());
 
 app.use(authRoutes);
 app.all('/api/*', ifLoggedIn, (req, res) => {
+    console.log(req)
     proxy.web(req, res);
 })
 
 proxy.on('proxyReq', (proxyReq, req, res, options) => {
-    proxyReq.setHeader('X-Special', 'Foobar');
+    if (req.body) {
+        const bodyData = JSON.stringify(req.body);
+        // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
+        proxyReq.setHeader('Content-Type','application/json');
+        proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+        // stream the content
+        proxyReq.write(bodyData);
+    }
 });
 
 app.listen(SERVER.PORT, () => {
